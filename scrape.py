057f18513a5fe1bca60682ebf67315d5e06e658c
Aslak Frafjord Skailand <aslaks16@uia.no>
2020-05-12 21:44:06 +0200

    scraped info from all players and post them to the server

--- scrape.py
+++ scrape.py
@@ -1,38 +1,73 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import requests
+import json
 
-driver = webdriver.Chrome('./chromedriver')
-
-class Player:
-    def __init__(self, number, name,  position):
-        self.name = name
-        self.number = number
-        self.position = position
+fotballno = "https://fotball.no"
+driverLeague = webdriver.Chrome('./chromedriver')
 
 players = []
 
+
+def showAllPlayers():
+    for player in players:
+        print('sending request....')
+        playerObj = {
+            "name": player.name,
+            "number": player.number,
+            "position": player.position,
+            "team": player.team
+        }
+        response = requests.post("http://localhost:8000/players", data = playerObj)
+        print(response.text)
+
+
+class Player:
+    def __init__(self, number,  name, position,  team):
+        if position is None:
+            position = None
+        self.name = name
+        self.number = number
+        self.position = position
+        self.team = team
+
+
 def createPlayer(scrapedData):
     playerInfo = []
-    for info in scrapedData:
-        info = info.split('\n')
-        for string in info:
-            string = string.strip()
-            playerInfo.append(string)
-    players.append(Player(playerInfo[0], playerInfo[1], playerInfo[2]))
+    for i in scrapedData:
+        if '\n\n' in i:
+            name = i.replace('\n\n', '')
+            playerInfo.append(name)
+            playerInfo.append(None)
+        else:
+            tempList = i.split('\n')
+            for j in tempList:
+                tempString = j.lstrip()
+                if tempString is not '':
+                    playerInfo.append(j.lstrip())
+    if(len(playerInfo) < 4):
+        playerInfo.insert(0, 0)
+    players.append(Player(playerInfo[0], playerInfo[1],  playerInfo[2], playerInfo[3]))
 
 def main():
-    driver.get("https://www.fotball.no/fotballdata/lag/spillere/?fiksId=145625")
-    content = driver.page_source
-    soup = BeautifulSoup(content)
-
-    for ul in soup.findAll('li', attrs={'class':'grid__item one-third players'}):
-        playerInfo = []
-        for span in ul.findAll('span', recursive=False):
-            playerInfo.append(span.text)
-        createPlayer(playerInfo)
-
-    for player in players:
-        print(player.name, player.number, player.position)
+    driverLeague.get("https://www.fotball.no/fotballdata/turnering/hjem/?fiksId=168930")
+    leagueContent = driverLeague.page_source
+    leagueSoup = BeautifulSoup(leagueContent, features="xml")
+    for table in leagueSoup.findAll('table', attrs={'class':'table'}):
+        for link in table.findAll('a'):
+            team = str(link.contents[0])
+            driverLeague.get(fotballno + link['href'].replace('hjem', 'spillere'))
+            content = driverLeague.page_source
+            soup = BeautifulSoup(content, features="xml")
+            # Scrape for player info
+            for li in soup.findAll('li', attrs={'class':'grid__item one-third players'}):
+                playerInfo = []
+                for span in li.findAll('span', recursive=False):
+                    playerInfo.append(span.text)
+                playerInfo.append(team)
+                createPlayer(playerInfo)
+    driverLeague.close()
+    showAllPlayers()
 
 if __name__ == "__main__":
     main()
